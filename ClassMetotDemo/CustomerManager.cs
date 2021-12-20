48319f083c7ae7af7f9069d8bfcea8124c86afdd
90553 <90553@DESKTOP-Q3HLI79>
2021-12-21 02:31:12 +0300

    asd

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added! : " + customer.Name);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted! : " + customer.Name);
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Customer Listed! : " + customer.Name);
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ilyas";
+            customer1.Surname = "Unver";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Ahmet";
+            customer2.Surname = "Mehmet";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Veli";
+            customer3.Surname = "Deli";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.List(customer3);
+
+        }
+    }
+}
